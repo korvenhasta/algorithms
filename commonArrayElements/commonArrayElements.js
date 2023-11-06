166431c363bae3c627a3270fcbb83d38eb246a00
Marina Kenina <avrora-lisa@yandex.ru>
2023-11-06 17:13:07 +0100

    Improve the solution

--- commonArrayElements/commonArrayElements.js
+++ commonArrayElements/commonArrayElements.js
@@ -1,12 +1,32 @@
 function common(a, b, c) {
-  let arrayOfCommonElements = [];
-  for (let i in a) {
-    if ((b.indexOf(a[i]) && c.indexOf(a[i])) > -1) {
-      arrayOfCommonElements.push(a[i]);
+  let dictionary = {};
+
+  for (let index = 0; index < a.length; index++) {
+    const element = a[index];
+    if (dictionary.hasOwnProperty(element) === false) {
+      dictionary[element] = [0, 0, 0];
     }
+    dictionary[element][0] = dictionary[element][0] + 1;
   }
-  return arrayOfCommonElements.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
+
+  for (let index = 0; index < b.length; index++) {
+    const element = b[index];
+    if (dictionary.hasOwnProperty(element) === false) {
+      continue;
+    }
+    dictionary[element][1] = dictionary[element][1] + 1;
+  }
+
+  for (let index = 0; index < c.length; index++) {
+    const element = c[index];
+    if (dictionary.hasOwnProperty(element) === false) {
+      continue;
+    }
+    dictionary[element][2] = dictionary[element][2] + 1;
+  }
+
+  return Object.keys(dictionary).reduce((accumulator, key) => {
+    return Number(key) * Math.min(...dictionary[key]) + Number(accumulator);
   }, 0);
 }
 
